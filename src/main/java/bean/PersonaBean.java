e981cb695a8a68facc86b08354ab7c8110ecb21f
Rodrigo Alcaino <rodrigo@quierosushi.cl>
2021-11-04 11:52:35 -0300

    first commit

---
+++ src/main/java/bean/PersonaBean.java
@@ -0,0 +1,47 @@
+package bean;
+
+import java.sql.Date;
+
+public class PersonaBean {
+
+	private String rut;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private Date fecha_ingreso;
+	
+	
+	public String getRut() {
+		return rut;
+	}
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public Date getFecha_ingreso() {
+		return fecha_ingreso;
+	}
+	public void setFecha_ingreso(Date fecha_ingreso) {
+		this.fecha_ingreso = fecha_ingreso;
+	}
+	
+	
+
+}
---
+++ src/main/webapp/userLogged.jsp
@@ -0,0 +1,28 @@
+   <%@ page language="java" 
+         contentType="text/html; charset=windows-1256"
+         pageEncoding="windows-1256"
+         import="bean.PersonaBean"
+   %>
+ 
+   <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+   <html>
+
+      <head>
+         <meta http-equiv="Content-Type" 
+            content="text/html; charset=windows-1256">
+         <title>   Usuario Logeado   </title>
+      </head>
+	
+      <body>
+
+         <center>
+            <% PersonaBean currentUser = (PersonaBean)session.getAttribute("currentSessionUser"); %>
+			
+            <h1> Bienvenido <%= currentUser.getNombre() + " " + currentUser.getApellido() %> </h1>
+         </center>
+
+      </body>
+	
+   </html>
